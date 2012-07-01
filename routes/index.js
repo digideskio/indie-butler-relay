98b26145f303fe729eeb8587158be4d4b5a00c4d
julien <julien.genestoux@gmail.com>
2012-07-01 15:23:35 -0700

    serving the right format too

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,23 @@
 /*
- * GET home page.
- */
+* GET home page.
+*/
 
 /*Catch all*/
 app.get('/:user/:path.:format', function(req, res){
   if(app.socketRouter.routeExists(req.params.user)) {
     app.socketRouter.route(req.params.user, req.params.path, req.params.format, function(data) {
-      res.json(data);
-    });
+      switch (format){
+        case 'json':
+          res.json(data);
+        break;
+        case 'html':
+          res.send(data);
+          break;
+      default :
+          res.send('Not Good');
+  }
+
+});
   }
   else {
     res.send('Not available :(', 503);
