b4cf81c75a4ca70e0e2435369b4283a3c58cc707
julien <julien.genestoux@gmail.com>
2012-07-01 21:37:51 -0700

    now expecting the extension itself to return the headers for every respons

--- routes/index.js
+++ routes/index.js
@@ -6,21 +6,14 @@
 app.get('/:user/:path.:format', function(req, res){
   if(app.socketRouter.routeExists(req.params.user)) {
     app.socketRouter.route(req.params.user, req.params.path, req.params.format, function(data) {
-      switch (req.params.format){
-        case 'json':
-          res.json(data);
-        break;
-        case 'atom':
-          res.send(data);
-        break;
-        case 'html':
-          res.send(data);
-          break;
-      default :
-          res.send('Not Good');
-  }
-
-});
+      if(data.headers) {
+        for(header in data.headers) {
+          res.header(header , data.headers[header]);
+        }
+      }
+      res.write(data.body);
+      res.end();
+    });
   }
   else {
     res.send('Not available :(', 503);
