419326c24e2b11ab32510b82b1610c51cee134d4
julien <julien.genestoux@gmail.com>
2012-07-03 23:28:28 -0700

    removed more logging messages

--- routes/authed.js
+++ routes/authed.js
@@ -1,35 +1,26 @@
 var http = require('http');
 
-app.get('/authed/:socket', function(req, res){
-        console.error("alpha");
-        console.info("alpha");
-        console.log("alpha");
-
+app.get('/authed/:socket', function(req, res) {
   http.get("http://indieauth.com/session?token=" + req.query.token, function (resp) {
-      console.error("0");
     var body = "";
     resp.on('data', function (chunk) {
       body += chunk
     });
     resp.on('end', function() {
-      console.error("1");
       auth = JSON.parse(body)
       var socket = app.socketRouter.sockets[req.params.socket];
       if(auth.me && socket) {
-        console.error("2");
         socket.handler = auth.me
         app.socketRouter.register(auth.me, socket);
         res.write("Awesome. You are now binded to " + auth.me);
       }
       else {
-              console.error("3");
         res.write("Nope. You are not binded to " + auth.me);
       }
       res.end();
     });
   }).on('error', function(e) {
-  console.log("Got error: " + e.message);
-});
-
-
+    res.write("Nope. You are not binded to " + auth.me);
+    res.end();
+  });
 });
