1fa0d97088f5363a9f5fd7de0b1ae6a9a56ead2e
julien <julien.genestoux@gmail.com>
2012-07-06 23:29:33 +0200

    better catch all route

--- routes/catchall.js
+++ routes/catchall.js
@@ -1,12 +1,12 @@
 
 /*Catch all*/
-app.get('/:path?', function(req, res){
+app.get('/*', function(req, res) {
+  var p = '/'
+  if (req.params[0]) {
+    p += req.params[0]
+  }
   var host = req.header('host').match(/butler\.(.*)/);
   if(host && host[1] && app.socketRouter.routeExists(host[1])) {
-    var p = '/'
-    if (req.params.path) {
-      p += req.params.path
-    }
     app.socketRouter.route(host[1], {headers: req.headers, path: p}, function(data) {
       for(header in data.headers) {
         res.header(header, data.headers[header]);
